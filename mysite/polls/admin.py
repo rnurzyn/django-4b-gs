d0b6ad627ad8adcd70a353ddc49a9d8aabe5ebd8
rnurzyn <marcin_izak@wp.pl>
2024-04-25 08:58:25 +0200

    koniec

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,3 +4,34 @@ from .models import Question,Choice
 
 admin.site.register(Question)
 admin.site.register(Choice)
+
+from django.contrib import admin
+
+from .models import Question
+
+
+
+admin.site.register(Question, QuestionAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
